ee32eecd88f710cb5bddb261ac07c4dfc2e98f13
LeeSez <109472918+LeeSez@users.noreply.github.com>
2022-09-26 09:21:06 +0300

    you can play and win
    you can nit do much and you also need to update this so it will actually be a puzzle

--- puzzle/main.js
+++ puzzle/main.js
@@ -1,23 +1,69 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+let pieces = [];
+let tray, box, winNote;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function initiate(){
+  tray = document.getElementById("tray");
+  box = document.getElementById("box");
+  winNote = document.getElementById("winNote");
 
-setupCounter(document.querySelector('#counter'))
+  createPieces();
+}
+
+function createPieces(){
+  for(let i = 1; i<26; i++){
+    let tempImg = document.createElement('img');
+    tempImg.src = "images/pieces/"+i+".png";
+    tempImg.id = i;
+
+    tempImg.style.width = "120px";
+    tempImg.style.height = "120px";
+    
+    tempImg.draggable="true";
+    tempImg.ondragstart= drag;
+    tempImg.droppble = "false";
+    
+    pieces.push(tempImg);
+  }
+  for(let i =0; i<pieces.length; i++){
+    let num1 = Math.floor(Math.random()*pieces.length);
+    let num2 = Math.floor(Math.random()*pieces.length);
+    
+    let temp = pieces[num1];
+    pieces[num1] = pieces[num2];
+    pieces[num2] = temp;
+  }
+  for(let i = 0; i<pieces.length; i++){
+    tray.appendChild(pieces[i]);
+  }
+}
+
+function allowDrop(event){
+  event.preventDefault();
+}
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+  if(event.target.id == "tray" || event.target.id =="box"){
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+    
+    winNote.style.visibility = checkWin() ? "visible": "hidden" ;
+  }
+}
+
+function checkWin(){
+  let array = box.childNodes;
+  if(box.childElementCount == 25){
+    for(let i=1; i<26; i++){
+      if(parseInt(array[array.length -i].id) != i)
+        return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+
